f0a7e25c653d43fed6f286f2508d8e8807c50cfc
Anurag <Mishra@Anurags-MacBook-Pro.local>
2018-07-11 15:12:04 +0530

    Fixing Selector

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -14,6 +14,9 @@ export interface State {
      todos: Todo[]
 }
 
+
+//---- Actions--------------- //
+
 const ADD_TODO = 'todo_app/todos/ADD_TODO';
 const MARK_DONE = 'todo_app/todos/MARK_DONE';
 const actionCreator = actionCreatorFactory();
@@ -21,9 +24,17 @@ const actionCreator = actionCreatorFactory();
 export const addTodo = actionCreator<string>(ADD_TODO);
 export const markDone = actionCreator<number>(MARK_DONE);
 
+
+
+//---- Reducers--------//
+
+
 const reducer = reducerWithInitialState([])
     .case(addTodo, (state, text) => ([ ...state, {done: false, id: nextId++, name: text, completed: false}]))
     .case(markDone, (state, rowid) => (state.map((todo) => (todo.id === rowid ? {...todo, done: !todo.done} : todo))))
 
 export default reducer;
 
+
+
+//---- Epics--------------- //
--- src/selectors/todos.ts
+++ src/selectors/todos.ts
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect'
 import {State} from "../reducers/todos";
 
+
 const getTodosState = ((state: State) => state.todos)
 
-export const getTodos = createSelector([getTodosState], s => s.todos)
+export const getTodos = createSelector([getTodosState], a => a);
+
